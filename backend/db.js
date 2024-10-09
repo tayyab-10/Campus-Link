9750827fffee9045f2b8b9e53b28d2f942f86231
SalmanNawaz921 <salmannawaz008@gmail.com>
2024-10-09 12:21:12 +0500

    made database online

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
-const dotenv=require("dotenv");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-dotenv.config({path:"Backend/Config/config.env"});
-
+dotenv.config({ path: "Backend/Config/config.env" });
 
 const connectToMongo = async () => {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("Connected to the database");
-    } 
-
+  console.log(process.env.NEW_MONGO_URI);
+  await mongoose.connect(process.env.NEW_MONGO_URI);
+  console.log("Connected to the database");
+};
 
 module.exports = connectToMongo;
