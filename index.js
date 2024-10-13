7c1d62fea40111cc0a79fb42dff8c6ca089fd2eb
SalmanNawaz921 <salmannawaz008@gmail.com>
2024-10-13 11:03:11 +0500

    added base get to check the backend is working

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-const connectToMongo = require('./db');
-const express = require('express');
-const errorMiddleware=require("./Middleware/Error");
-const dotenv=require("dotenv");
-const cors = require('cors');
+const connectToMongo = require("./db");
+const express = require("express");
+const errorMiddleware = require("./Middleware/Error");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-connectToMongo()
+connectToMongo();
 
-dotenv.config({path:"backend/Config/config.env"});
-const port=process.env.PORT;
+dotenv.config({ path: "backend/Config/config.env" });
+const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 // CORS Configuration
-const allowedOrigins = process.env.FRONTEND_URL.split(','); // Split the comma-separated list
+const allowedOrigins = process.env.FRONTEND_URL.split(","); // Split the comma-separated list
 
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      // If the origin is in the allowed list, allow it
-      callback(null, true);
-    } else {
-      // Otherwise, block the request
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true // Allow credentials (cookies, authorization headers, etc.)
-}));
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
 
-  app.use(errorMiddleware);
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        // If the origin is in the allowed list, allow it
+        callback(null, true);
+      } else {
+        // Otherwise, block the request
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
 
-//Available Routes 
+app.use(errorMiddleware);
 
-app.use('/api/auth', require("./Routes/UserRoute"));
+//Available Routes
 
+app.use("/api/auth", require("./Routes/UserRoute"));
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`)
-})
-
+  console.log(`Server is listening at http://localhost:${port}`);
+});
