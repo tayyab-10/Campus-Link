82631d829a1be54398fc7889e52ba15aedf66e3e
SalmanNawaz921 <salmannawaz008@gmail.com>
2024-10-15 20:40:00 +0500

    fixed minor bug in event route

--- Routes/EventRoute.js
+++ Routes/EventRoute.js
@@ -3,7 +3,7 @@ const router=express.Router();
 const { createEvent,deleteEvent,updateEvent, getEventsForSociety } = require("../Controllers/EventController");
 const { isAuthenticatedUser } = require("../Middleware/auth");
 
-router.route("/createEvent").post(isAuthenticatedUser,createEvent);
+router.route("/createEvent/:societyId").post(isAuthenticatedUser,createEvent);
 router.route("/deleteEvent/:eventId").delete(isAuthenticatedUser,deleteEvent);
 router.route("/updateEvent/:eventId").patch(isAuthenticatedUser,updateEvent);
 router.route("/getEventsForSociety/:societyId").post(isAuthenticatedUser,getEventsForSociety);
