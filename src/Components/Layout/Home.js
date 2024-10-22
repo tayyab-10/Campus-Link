e194a9ecffbe57ce9e06293783735225495ca19b
SalmanNawaz921 <salmannawaz008@gmail.com>
2024-10-22 20:02:57 +0500

    just formatted the home file

--- src/Components/Layout/Home.js
+++ src/Components/Layout/Home.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from 'react';
-import SocietyCard from '../Societies/Societycard';
+import React, { useEffect } from "react";
+import SocietyCard from "../Societies/Societycard";
 import Navbar from "../Layout/navbar";
-import { useDispatch, useSelector } from 'react-redux';
-import { useAlert } from 'react-alert';
-import { clearError, getSocieties } from '../../Actions/societyactions';
-import Loader from '../Loader/Loader';
+import { useDispatch, useSelector } from "react-redux";
+import { useAlert } from "react-alert";
+import { clearError, getSocieties } from "../../Actions/societyactions";
+import Loader from "../Loader/Loader";
 
 const LandingPage = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
 
-  const { error, loading, societies = [] } = useSelector((state) => state.GetSocieties);
+  const {
+    error,
+    loading,
+    societies = [],
+  } = useSelector((state) => state.GetSocieties);
   console.log("Societies in component: ", societies);
   
   useEffect(() => {
@@ -24,23 +28,22 @@ const LandingPage = () => {
   return (
     <>
       <Navbar />
-      {loading?(<Loader/>):(
-
-      <div className="min-h-screen bg-gray-50 p-6 md:p-12 lg:p-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-          
-        {societies && societies.length > 0 ? (
-            societies.map((society) => (
-              <SocietyCard key={society._id} society={society} />
-            ))
-          ) : (
-            <p>No societies available.</p>
-          )}
-
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="min-h-screen bg-gray-50 p-6 md:p-12 lg:p-20">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+            {console.log(societies)}
+            {societies && societies.length > 0 ? (
+              societies.map((society) => (
+                <SocietyCard key={society._id} society={society} />
+              ))
+            ) : (
+              <p>No societies available.</p>
+            )}
+          </div>
         </div>
-      </div>
       )}
-
     </>
   );
 };
